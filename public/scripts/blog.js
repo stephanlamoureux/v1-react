1f8859b9402421dc0f6a4c28e0beeef38df66153
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-12-04 17:47:48 -0500

    copied v1 files over

---
+++ public/scripts/blog.js
@@ -0,0 +1,68 @@
+const api_url = 'https://dev.to/api/articles?username=stephanlamoureux'
+
+async function getArticle() {
+  const response = await fetch(api_url)
+  const data = await response.json()
+  const title = data[0].title
+  const link = data[0].url
+  const tag = data[0].tag_list
+  const name = data[0].user.username
+  // const profilePic = data[0].user.profile_image
+  // const cover = data[0].cover_image
+
+  document.getElementById('article_title').textContent = title
+  document.getElementById('article_link').href = link
+  document.getElementById('tags').textContent = tag.map(i => '#' + i)
+  document.getElementById('username').textContent = '@' + name
+  // document.getElementById('profile_image').src = profilePic
+
+  //For multiple articles that you want to display
+  function displayMultipleArticles() {
+    for (let i = 1; i < 4; i++) {
+      let element = document.createElement('div') //container
+      element.className = 'devArticle'
+
+      let articleLink = document.createElement('a') //link
+      articleLink.id = 'article_link' + i
+      articleLink.target = '_blank'
+      articleLink.href = data[i].url
+
+      let theTitle = document.createElement('h1') //title
+      theTitle.id = 'article_title'
+      theTitle.textContent = data[i].title
+
+      let infoArt = document.createElement('div') //info container
+      infoArt.className = 'article-info'
+
+      // let emptyProfileDiv = document.createElement('div') //pfp container
+      // let theProfileImage = document.createElement('img') //pfp
+      // theProfileImage.style.width = '50px'
+      // theProfileImage.id = 'profile_image'
+      // theProfileImage.src = data[i].user.profile_image
+
+      let emptyDiv = document.createElement('div') //tags container
+
+      let tagsInArt = document.createElement('p') //tag
+      tagsInArt.id = 'tags'
+      let realTags = data[i].tag_list
+      tagsInArt.textContent = realTags.map(i => '#' + i)
+
+      let yourUsername = document.createElement('p') //username
+      yourUsername.id = 'username'
+      yourUsername.textContent = '@' + data[i].user.username
+
+      element.appendChild(articleLink)
+      articleLink.appendChild(theTitle)
+      articleLink.appendChild(infoArt)
+      // infoArt.appendChild(emptyProfileDiv)
+      // emptyProfileDiv.appendChild(theProfileImage)
+      infoArt.appendChild(emptyDiv)
+      emptyDiv.appendChild(tagsInArt)
+      emptyDiv.appendChild(yourUsername)
+      document.getElementById('blog_container').appendChild(element)
+    }
+  }
+
+  displayMultipleArticles()
+}
+getArticle()
---
+++ public/scripts/main.js
@@ -0,0 +1,48 @@
+// Hamburger Menu
+
+const navbar = document.querySelector('.navbar')
+const menu = document.querySelector('.menu')
+
+menu.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+  navbar.classList.toggle('showNav')
+  menu.classList.toggle('showClose')
+}
+
+const menuLinks = document.querySelectorAll('.menu-link')
+
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener('click', toggleMenu)
+})
+
+// Typed.js
+
+var typed = new Typed('#typed', {
+  strings: [
+    'A Frontend Fanatic.',
+    "^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+    '^2000 An Over-Caffeinated Coder.',
+    '^2000 The Duke of Dark Mode.',
+  ],
+  typeSpeed: 70,
+  loop: true,
+  startDelay: 1000,
+  backDelay: 3000,
+  backSpeed: 50,
+})
+
+// About section hover effect
+
+const links = document.querySelectorAll('.tech-used-link')
+
+links.forEach(link => {
+  const bullet = link.parentElement.querySelector('.bullet')
+  link.addEventListener('mouseover', () => {
+    bullet.style.color = 'var(--dracula-purple)'
+    bullet.style.transition = 'ease 0.1s'
+  })
+  link.addEventListener('mouseleave', () => {
+    bullet.style.color = 'var(--dracula-foreground)'
+  })
+})
