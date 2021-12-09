bde4a14ff046c476ebe80fef49e51b7fd788376a
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-12-09 13:14:01 -0500

    added typedjs

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typed from 'react-typed'
 
 export default function Nav() {
 	return (
@@ -40,7 +41,21 @@ export default function Nav() {
 						<span className="show header-gradient">L.</span>
 					</h1>
 					<p className="header-sub">
-						<span id="typed" className="typed"></span>
+						<span id="typed" className="typed">
+							<Typed
+								strings={[
+									'A Frontend Fanatic.',
+									"^2000 The <span class='typed-gradient'>Gradient</span> Gangsta.",
+									'^2000 An Over-Caffeinated Coder.',
+									'^2000 The Duke of Dark Mode.',
+								]}
+								typeSpeed={70}
+								loop={true}
+								startDelay={1000}
+								backDelay={3000}
+								backSpeed={50}
+							/>
+						</span>
 					</p>
 				</div>
 				<button className="menu" aria-label="Main Menu"></button>
